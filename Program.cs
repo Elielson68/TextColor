783a6ac33d2f530eb4fe3a8198fe3f1dcaec1885
Elielson68 <elielsonbr.com@gmail.com>
2021-06-24 15:32:00 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Coloracao;
+using System.Threading;
+namespace Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //TextoColorido.Imprimir("(.)(.)dada(B=White|L=Black)");
+            /* TextoColorido.BarraCarregamento();
+            TextoColorido.Imprimir("\n\nAgora é possível imprimir texto  (B=White|L=Black)com fundo preto e letra branca(.) ou (B=Red|L=Yellow)destacado de vermelho com amarelo(.) ou se preferir  uma palavra vai colorida (B=White|L=Black)C(.)(B=Red|L=Yellow)o(.)(B=Magenta|L=White)m(.)(B=Cyan|L=Yellow)o(.) (B=Black|L=Blue)essa(.).\n");
+            Thread.Sleep(10000);
+            TextoColorido.BarraCarregamento("Here go!", "Red");
+            Console.WriteLine("\n");
+            TextoColorido.Mario();   */ 
+            TextoColorido.ArcoIris("aaaaaaaaa");
+
+        }
+    }
+}
